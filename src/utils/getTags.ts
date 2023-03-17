a88e2978dae4567f0bfcc0eee77f43f43a36f614
Miko Ademagic <miko.ademagic@gmail.com>
2023-03-17 16:11:58 +1100

    giving getTags some steroids

--- src/utils/getTags.ts
+++ src/utils/getTags.ts
@@ -3,5 +3,25 @@ type Post = {
 }
 
 export default function getTags(posts: Post[]) {
-  return [...new Set(posts.map(p => p.tags).flat())];
+  // const tagsArr = [...new Set(posts.map(p => p.tags).flat())];
+  const tagsPerPost = posts.map(p => p.tags);
+  const tagsArr = tagsPerPost.flat();
+  const tagsUnique = new Set(tagsArr);
+  const tagCount = {}
+
+  for (const tag of tagsArr) {
+    if (tagCount[tag]) {
+      tagCount[tag] += 1;
+    } else {
+      tagCount[tag] = 1;
+    }
+  }
+
+  const output = {
+    tags: [...tagsUnique],
+    tagCount,
+    total: tagsUnique.size,
+  };
+
+  return output;
 }
