eb7d7a5b617c43f4a2ccb62ae2ba3ca06c2fadc0
Miko Ademagic <miko.ademagic@gmail.com>
2023-05-16 06:34:25 +1000

    Upgrade to Astro 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,18 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://miko.ademagic.com',
+  site: 'http://miko.ademagic.com/',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: 'nord'
     },
-    rehypePlugins: [
-      [
-        'rehype-external-links',
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
-})
+    rehypePlugins: [['rehype-external-links', {
+      target: '_blank'
+    }]]
+  }
+});
