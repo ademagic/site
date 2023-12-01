e58072887f19e83bf7b99a0eaee6e642bbd9c74f
Miko Ademagic <miko.ademagic@gmail.com>
2023-12-01 11:42:17 +1100

    converting blog page to use content collections

--- src/utils/getPostData.ts
+++ src/utils/getPostData.ts
@@ -2,13 +2,13 @@ import readingTime from 'reading-time'
 
 type Post = {
   title: string
-  file: string
-  rawContent: () => string
+  slug: string
+  body: string
 }
 
 export default function getPostData(post: Post) {
   return {
-    slug: post.file.split('/').pop().split('.').shift(),
-    readingTime: readingTime(post.rawContent()).text,
+    slug: post.slug,
+    readingTime: readingTime(post.body).text,
   }
 }
