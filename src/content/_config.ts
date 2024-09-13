599a3c2d8b8d3d92d70b21063efbe6313a5f97d8
Miko Ademagic <miko.ademagic@gmail.com>
2024-09-13 14:11:32 +1000

    blogroll page

--- src/content/_config.ts
+++ src/content/_config.ts
@@ -8,7 +8,18 @@ const blogCollection = defineCollection({
 const releasesCollection = defineCollection({
 });
 
+const blogRollCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    url: z.string(),
+    dateAdded: z.string(),
+    description: z.string().optional(),
+    rss: z.string().optional(),
+  })
+});
+
 export const collections = {
   blog: blogCollection,
   releases: releasesCollection,
+  blogroll: blogRollCollection,
 };
